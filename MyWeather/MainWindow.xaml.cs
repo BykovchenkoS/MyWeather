fc5de68470270be46389e7a1c602a7ef4cafe8b9
Sonya Bykovchenko <sony.bykovchenko@gmail.com>
2023-12-10 13:24:45 +0300

    the beginning of the layout of the main window

--- MyWeather/MainWindow.xaml.cs
+++ MyWeather/MainWindow.xaml.cs
@@ -15,14 +15,31 @@ using System.Windows.Shapes;
 
 namespace MyWeather
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Border_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void textSearch_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtSearch.Focus();
+        }
+
+        private void txtSearch_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtSearch.Text) && txtSearch.Text.Length > 0)
+                textSearch.Visibility = Visibility.Collapsed;
+            else
+                textSearch.Visibility = Visibility.Visible;
+        }
+
     }
 }
